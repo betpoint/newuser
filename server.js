b450de61a01156b4e1c269fd461fa01a152fc324
betpoint <158754485+betpoint@users.noreply.github.com>
2025-10-18 15:23:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const pool = new Pool({
   connectionString:
     "postgres://postgres.iznxrukdqbrcxjzhvwyk:ZHITafYu6WJqNqjJ@aws-0-us-west-1.pooler.supabase.com:5432/postgres",
-  ssl: { rejectUnauthorized: true }, // validate SSL certificate
+  ssl: { rejectUnauthorized: false }, // validate SSL certificate
 });
 
 // ✅ OneSignal configuration
